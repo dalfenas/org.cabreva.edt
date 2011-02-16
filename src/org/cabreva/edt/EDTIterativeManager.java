b9661c25850ab7aa02e0ee4318bc940e0119d2b9
lukarios <lukariosbertin@gmail.com>
2011-02-16 15:18:26 -0200

    correcao na classe EDTIterativeManager.class para ser chamada pelo ExecutorTesteValidacao

--- src/org/cabreva/edt/EDTIterativeManager.java
+++ src/org/cabreva/edt/EDTIterativeManager.java
@@ -10,58 +10,66 @@ import org.jdom.input.SAXBuilder;
 
 public class EDTIterativeManager {
 
-	private String workflowFileName;
-	private String outputFolderName;
-	EDTWorkflow wf;
-	EDTTransaction transaction = null;
-	private SAXBuilder builder;
+    private String workflowFileName;
+    private String outputFolderName;
+    EDTWorkflow wf;
+    EDTTransaction transaction = null;
+    private SAXBuilder builder;
 
-	public EDTIterativeManager(String workflowFile, String outputFolder) {
-		super();
-		this.workflowFileName = workflowFile;
-		this.outputFolderName = outputFolder;
-	}
+    public EDTIterativeManager(String workflowFile, String outputFolder) {
+        super();
+        this.workflowFileName = workflowFile;
+        this.outputFolderName = outputFolder;
+    }
 
-	public void init() throws JDOMException, IOException, EDTException {
-		// Inicializando do JDOM
-		builder = new SAXBuilder();
+    public void init() throws JDOMException, IOException, EDTException {
+        // Inicializando do JDOM
+        builder = new SAXBuilder();
 
-		// Parsing do workflow
-		EDTWorkflow wf = parseFile(new File(workflowFileName));
-		// initicamos a transacao
-		transaction = new SeleniumTransaction();
-		transaction.start(wf.getDomain());
-		try {
-			// iniciamos o workflow
-			wf.init(new EDTContext(transaction, outputFolderName));
-		} finally {
-			// paramos a transacaoo antes de sair por erro
-			stop();
-		}
+        // Parsing do workflow
+        wf = parseFile(new File(workflowFileName));
+        // initicamos a transacao
+        transaction = new SeleniumTransaction();
+        transaction.start(wf.getDomain());
+        try {
+            // iniciamos o workflow
+            wf.init(new EDTContext(transaction, outputFolderName));
+        } catch (Exception ex) {
+            stop();
+        }/*finally {
+        // paramos a transacaoo antes de sair por erro
+        stop();
+        }*/ 
 
-	}
 
-	public void run(String fullFileName) throws JDOMException, IOException {
-		try {
-			EDTContext caseContext = new EDTContext(transaction, outputFolderName, new EDTTestCase(new File(
-					fullFileName)));
-			wf.run(caseContext);
-		} finally {
-			stop();
-		}
-	}
+    }
 
-	public void stop() {
-		transaction.stop();
-		transaction = null;
-		wf = null;
-	}
+    public void run(String fullFileName) throws JDOMException, IOException {
+        try {
+            EDTContext caseContext = new EDTContext(transaction, outputFolderName, new EDTTestCase(new File(
+                    fullFileName)));
+            wf.run(caseContext);
+        } //paramos a transacao caso ocorra uma excecao
+        catch (Exception ex) {
+            stop();
+        }
+        /*finally {
+        stop();
+        }*/
 
-	private EDTWorkflow parseFile(File file) throws JDOMException, IOException {
-		Document doc = builder.build(file.getAbsoluteFile());
-		EDTWorkflow workflow = new EDTWorkflow();
-		workflow.parseRoot(doc.getRootElement());
-		doc.detachRootElement();
-		return workflow;
-	}
+    }
+
+    public void stop() {
+        transaction.stop();
+        transaction = null;
+        wf = null;
+    }
+
+    private EDTWorkflow parseFile(File file) throws JDOMException, IOException {
+        Document doc = builder.build(file.getAbsoluteFile());
+        EDTWorkflow workflow = new EDTWorkflow();
+        workflow.parseRoot(doc.getRootElement());
+        doc.detachRootElement();
+        return workflow;
+    }
 }
